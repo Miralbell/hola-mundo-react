795407e5ecccda01ddb33c0bd3803d4db4edbeae
Miralbell <59009482+Miralbell@users.noreply.github.com>
2021-08-13 22:20:20 +0200

    Estudio de Props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -13,21 +13,42 @@ class Hello extends Component {
   render () {
     return <h2>{this.props.title}</h2>
   }
-}
+};
+
+
+class Text extends Component {
+  render () {
+    const { 
+      arrayOfNumbers, 
+      muliply, 
+      objectWithInfo,
+      Title,
+    } = this.props
+    const mappedNumbers = arrayOfNumbers.map(this.props.muliply)
+    return <div>
+      {Title} 
+      <p>{mappedNumbers.join(', ')}</p>
+      <p>{objectWithInfo.Key1}</p>
+      </div>
+  }
+};
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Hello title = 'Hello From Props'/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Text
+        arrayOfNumbers = {[2,3,10]} 
+        objectWithInfo = {{Key1:'Value', Key2:'other'}}
+        isActivated 
+        muliply = {(number) => number * 4}
+        number={2} 
+        Text = 'Texto en String'
+        Title = {<h1>Este es el Titulo</h1>}
+        />
+        
       </header>
     </div>
   );

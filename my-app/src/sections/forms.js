446ee0950dec5c819256b67d0d965795aa9b590b
Miralbell <59009482+Miralbell@users.noreply.github.com>
2021-08-18 10:08:59 +0200

    formularios en react

--- my-app/src/sections/forms.js
+++ my-app/src/sections/forms.js
@@ -1,32 +1,60 @@
 import React, { Component } from 'react'
 
 export default class Forms extends Component {
-    handleClick (e) {
-        const name = document.getElementById('name').value
-        const email = document.getElementById('twitter').value 
-        console.log({name,email})
+    constructor () {
+        super()
+        this.state = {
+            inputName: '', 
+            inputTwitter: '@',
+            inputTerms: 'true',
+        }
+    }
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state)
 
     }
+    handleChange = (e) => {
+        console.log('handleChange')
+        console.log(e.target.checked)
+        this.setState({inputTerms: e.target.checked})
+    }
+
     render () {
         return (
             <div>
                 <h4>Formularios</h4>
-                <form>
+                <form onSubmit={this.handleSubmit} >
                     <p>
-                        <label>Nombre :</label>
+                        <label htmlFor='name'>Nombre :</label>
                         <input
                         id='name'
                         name='User Name'
-                        placeholder='introduce el nombre'/>
+                        onChange={e => this.setState({inputName: e.target.value})}
+                    placeholder='introduce el nombre'
+                        ref={inputElement => this.inputName = inputElement}
+                        value={this.state.inputName}/>
                     </p>
                     <p>
                         <label>Twitter :</label>
                         <input
                         id='twitter'
                         name='twitteraccount'
-                        placeholder='introduce el twitter'/>
+                        placeholder='introduce el twitter'
+                        value={this.state.inputTwitter}
+                        onChange={e => this.setState({inputTwitter: e.target.value})}/>
                     </p>
-                    <button onClick= {this.handleClick}>Enviar</button>
+                    <p>
+                        <label>Terminos y condiciones</label>
+                        <input
+                        type='checkbox'
+                        onChange={this.handleChange}
+                        id='twitter'
+                        name='twitteraccount'
+                        placeholder='introduce el twitter'
+                        checked={this.state.inputTerms}/>/>
+                    </p>
+                    <button>Enviar</button>
                 </form>
             </div>
         )

df149b68f2ef84f998f99ea439f8ec9ad22fde5d
Miralbell <59009482+Miralbell@users.noreply.github.com>
2021-08-22 13:01:09 +0200

    aprendiendo a hacer fetch api

--- my-app/src/sections/components/FetchExample.js
+++ my-app/src/sections/components/FetchExample.js
@@ -1,3 +1,35 @@
+import { object } from 'prop-types';
 import React, { Component }  from 'react';
 
 export default class FetchExample extends Component {
+    state = { bpi:{}}
+    componentDidMount () {
+        fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+        .then(res => res.json())
+        .then(data => {
+            const { bpi } = data
+            this.setState({ bpi })
+        })
+    }
+    _renderCurrencies () {
+        const { bpi } = this.state
+        const currencies = Object.keys(bpi)
+        return currencies.map(currency => {
+            return (
+            <div key={currency}>
+                1 BTC is {bpi[currency].rate}
+            <span>{currency}</span>
+            </div>
+            )
+        })
+    }
+
+    render () {
+        return (
+            <div>
+                <h1>Bitcoin Price Index</h1>
+                {this._renderCurrencies()}
+            </div>
+        )
+    }
+}
